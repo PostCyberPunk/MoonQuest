595e7e5ce0a1b625fa13cb196655f4b69937bd15
PostCyberPunk <lyhguest2@outlook.com>
2024-05-05 15:02:08 +0800

    feat:LimeLog debug

--- libmoon/src/main/java/com/limelight/LimeLog.java
+++ libmoon/src/main/java/com/limelight/LimeLog.java
@@ -18,9 +18,25 @@ public class LimeLog {
     public static void severe(String msg) {
         LOGGER.severe(msg);
     }
+
+    private static final boolean DEBUGING = false;
+    private static final boolean VERBOSE = false;
+
     public static void todo(String msg) {
         LOGGER.severe(msg);
     }
+
+    public static void debug(String msg) {
+        if (DEBUGING)
+            LOGGER.warning(msg);
+    }
+
+    //TDOO: add a verbose level
+    public static void verbose(String msg) {
+        if (VERBOSE)
+            LOGGER.info("verbose:" + msg);
+    }
+
     public static void setFileHandler(String fileName) throws IOException {
         LOGGER.addHandler(new FileHandler(fileName));
     }
