e8628c22621f548ce2f9185ea0c3c23bfff0c380
PostCyberPunk <lyhguest2@outlook.com>
2024-05-08 10:29:21 +0800

    add to data for list object

--- limelight_plugin/liblime/src/main/java/com/liblime/types/AppObject.java
+++ limelight_plugin/liblime/src/main/java/com/liblime/types/AppObject.java
@@ -1,6 +1,9 @@
 package com.liblime.types;
 
+import com.google.gson.Gson;
 import com.limelight.nvstream.http.NvApp;
+
+import java.io.Serializable;
 
 public class AppObject {
     public final NvApp app;
@@ -16,5 +19,24 @@ public class AppObject {
     @Override
     public String toString() {
         return app.getAppName();
+    }
+
+    public String ToData() {
+        return new Gson().toJson(new AppData(app));
+    }
+
+    private static class AppData implements Serializable {
+        public String appName;
+        public int appId = -1;
+        public boolean initialized = false;
+
+        public AppData(NvApp app) {
+            if (app == null) {
+                throw new IllegalArgumentException("app must not be null");
+            }
+            this.appName = app.getAppName();
+            this.appId = app.getAppId();
+            this.initialized = app.isInitialized();
+        }
     }
 }
--- limelight_plugin/liblime/src/main/java/com/liblime/types/ComputerObject.java
+++ limelight_plugin/liblime/src/main/java/com/liblime/types/ComputerObject.java
@@ -1,6 +1,10 @@
 package com.liblime.types;
 
+import com.google.gson.Gson;
 import com.limelight.nvstream.http.ComputerDetails;
+import com.limelight.nvstream.http.PairingManager;
+
+import java.io.Serializable;
 
 public class ComputerObject {
     public ComputerDetails details;
@@ -15,5 +19,23 @@ public class ComputerObject {
     @Override
     public String toString() {
         return details.name;
+    }
+
+    public String ToData() {
+        return new Gson().toJson(new ComputerData(details));
+    }
+
+    public static class ComputerData implements Serializable {
+        public String uuid;
+        public String name;
+        public ComputerDetails.State state;
+        public PairingManager.PairState pairState;
+
+        public ComputerData(ComputerDetails details) {
+            this.uuid = details.uuid;
+            this.name = details.name;
+            this.state = details.state;
+            this.pairState = details.pairState;
+        }
     }
 }
